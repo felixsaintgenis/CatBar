d1ebf4be245d62c70b1455669cb008e93500cc40
Headwhate <t.wiart@outlook.fr>
2017-08-04 00:18:11 +0200

    update lacarte.html .scss

--- javascripts/lacarte.js
+++ javascripts/lacarte.js
@@ -2,25 +2,32 @@ $(document).ready(function() {
 	$("#tout_la_carte").click(function(){
 	$("a").removeClass("active");
 	$("#tout_la_carte").addClass("active");
-    $(".tout_la_carte").show("swing");
+    $(".tout_la_carte").show("slow");
 });
 	$("#petit_dej").click(function(){
 	$("a").removeClass("active");
 	$("#petit_dej").addClass("active");
-	$('.tout_la_carte').hide("swing");
-    $(".petit_dej").show("swing");
+	$('.tout_la_carte').hide("slow");
+    $(".petit_dej").show("slow");
 });
 
 $("#dej").click(function(){
 	$("a").removeClass("active");
 		$("#dej").addClass("active");
-	$('.tout_la_carte').hide("swing");
-    $(".dej").show("swing");
+	$('.tout_la_carte').hide("slow");
+    $(".dej").show("slow");
 });
 $("#diner").click(function(){
 	$("a").removeClass("active");
 	$("#diner").addClass("active");
-	$('.tout_la_carte').hide("swing");
-    $(".diner").show("swing");
+	$('.tout_la_carte').hide("slow");
+    $(".diner").show("slow");
 });
+$("#cocktails").click(function(){
+	$("a").removeClass("active");
+	$("#cocktails").addClass("active");
+	$('.tout_la_carte').hide("slow");
+    $(".cocktails").show("slow");
+});
+
 });
