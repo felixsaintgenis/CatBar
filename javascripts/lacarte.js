cc8f51a3d1d23cd20a6bb15767f683c4448ee269
Headwhate <t.wiart@outlook.fr>
2017-08-03 12:05:46 +0200

    need images and plate names

---
+++ javascripts/lacarte.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+	$("#petit_dej").click(function(){
+	$("a").removeClass("active");
+	$("#petit_dej").addClass("active");
+	$('.tout_la_carte').hide("slow");
+    $(".petit_dej").show("slow");
+});
+$("#tout_la_carte").click(function(){
+	$("a").removeClass("active");
+	$("#tout_la_carte").addClass("active");
+    $(".tout_la_carte").show("slow");
+});
+$("#dej").click(function(){
+	$("a").removeClass("active");
+		$("#dej").addClass("active");
+	$('.tout_la_carte').hide("slow");
+    $(".dej").show("slow");
+});
+$("#diner").click(function(){
+	$("a").removeClass("active");
+	$("#diner").addClass("active");
+	$('.tout_la_carte').hide("slow");
+    $(".diner").show("slow");
+});
+});
