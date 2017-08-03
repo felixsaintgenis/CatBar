fc7ae67e92865146bac91a246b0182a3bc8c007d
NicolasMartinho <nicolas.martinho@outlook.fr>
2017-08-03 11:13:30 +0200

    finished panels

--- javascripts/cat.js
+++ javascripts/cat.js
@@ -1,12 +1,17 @@
 const catCard = (e) => {
-  let tar = e.target;
-  if (tar.style.height == "10em") {
-    window.innerWidth>800?tar.style.height = "60em":tar.style.height = "30em"
-  }
-  else {
+  let tar;
+  e.target.localName == "article"?tar = e.target:e.target.parentNode.localName== "article"?tar = e.target.parentNode:tar = e.target.parentNode.parentNode;
+  if (tar.style.height > "10em"){
     tar.style.height = "10em"
   }
+  else {
+    window.innerWidth>800?tar.style.height = "50em":tar.style.height = "30em";
+
+  }
+  tar.children['1'].classList.toggle('cat-left')
+  tar.children['2'].classList.toggle('cat-right')
+  tar.firstElementChild.classList.toggle('cat-invi')
 }
-console.log(window.innerWidth);
+
 const catArr = Array.from(document.querySelectorAll('.cat-card'))
 catArr.forEach(cat => cat.addEventListener('click', catCard))
